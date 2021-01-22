55d982715c412fd13b80981151830b6b8a84ac52
Didier <didorg@gmail.com>
2021-01-22 18:24:54 -0500

    add Todo App

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,10 +1,31 @@
+import React, { Component } from 'react';
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some fruit' },
+      { id: 2, content: 'buy some fruit' }
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      // (key and value has the same name)
+      // todos: todos
+      todos
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
